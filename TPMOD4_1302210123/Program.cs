4e6954ccbcaa54ee70587da4c1b6d4edaeb648f6
unknown <soulreaper2407@gmail.com>
2023-03-14 16:59:24 +0700

    menambahkan class posisikaraktergame

--- TPMOD4_1302210123/Program.cs
+++ TPMOD4_1302210123/Program.cs
@@ -19,6 +19,53 @@ namespace TPMOD4_1302210123
                 return kodeBuah[(int)kode];
             }
         }
-        
+        public enum State { JONGKOK, BERDIRI, TENGKURAP, TERBANG };
+        public enum Trigger { TombolS, TombolW, TombolX };
+
+        class transition
+        {
+            public State prevState;
+            public State nextState;
+            public Trigger trigger;
+            public transition(State prevState, State nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+        transition[] transitions =
+        {
+        new transition(State.JONGKOK, State.TENGKURAP, Trigger.TombolS),
+        new transition(State.TENGKURAP, State.JONGKOK, Trigger.TombolW),
+        new transition(State.BERDIRI, State.JONGKOK, Trigger.TombolS),
+        new transition(State.JONGKOK, State.BERDIRI, Trigger.TombolW),
+        new transition(State.BERDIRI, State.TERBANG, Trigger.TombolW),
+        new transition(State.TERBANG, State.BERDIRI, Trigger.TombolS),
+        new transition(State.TERBANG, State.JONGKOK, Trigger.TombolX)
+        };
+
+        public State currentState;
+
+        public State getNextState(State prevState, Trigger trigger)
+        {
+            State nextState = prevState;
+            for (int i = 0; i < transitions.Length; i++)
+            {
+                if (transitions[i].prevState == prevState && transitions[i].trigger == trigger)
+                    nextState = transitions[i].nextState;
+            }
+            return nextState;
+        }
+        public void activateTrigger(Trigger trigger)
+        {
+            State nextState = getNextState(currentState, trigger);
+            currentState = nextState;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
